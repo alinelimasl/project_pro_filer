ac4f2817d52451b81b53e46655871651e118b6b4
Aline <aline_lima@outlook.com>
2024-04-01 14:31:43 -0300

    Requisito 5 em parte, e o 6

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -1,1 +1,36 @@
 from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
+import pytest
+
+
+@pytest.fixture
+def example_files(tmp_path):
+    path_one_output = tmp_path / "folder1"
+    path_two_output = tmp_path / "folder2"
+    path_three_output = tmp_path / "file1.txt"
+    path_one_output.mkdir()
+    path_two_output.mkdir()
+    path_three_output.touch()
+    path_three_output.write_text("test content")
+    example_text_one = path_one_output / "text1.txt"
+    example_text_two = path_two_output / "text1.txt"
+    example_text_one.touch()
+    example_text_two.touch()
+    return [
+        str(example_text_one),
+        str(example_text_two),
+        str(path_three_output),
+    ]
+
+
+def test_different_exception_files():
+    except_context = {"all_files": ["file1.py", "file2.py", "file3.py"]}
+    with pytest.raises(ValueError):
+        find_duplicate_files(except_context)
+
+
+def test_different_duplicates_files(example_files):
+    duplicated_context = {"all_files": example_files}
+    resulted = find_duplicate_files(duplicated_context)
+    assert len(resulted) == 1
+    assert "text1.txt" in resulted[0][0]
+    assert "text1.txt" in resulted[0][1]
--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,42 @@
+import pytest
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+
+
+import os
+
+
+@pytest.fixture
+def context(tmp_path):
+    test_dir = tmp_path / "src"
+    test_dir.mkdir()
+
+    file1 = test_dir / "app.py"
+    file1.write_text("conteúdo do arquivo 1")
+
+    file2 = test_dir / "__init__.py"
+    file2.write_text("conteúdo do arquivo 2")
+
+    total_size = sum(os.path.getsize(file) for file in [file1, file2])
+
+    return {
+        "all_files": [
+            "app.py",
+            "__init__.py",
+        ],
+        "total_size": total_size,
+    }
+
+
+def test_show_disk_usage_with_files(context, capsys):
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert f"Total size: {context['total_size']}" in captured.out
+    assert "src/app.py" in captured.out
+    assert "src/__init__.py" in captured.out
+
+
+def test_show_disk_usage_without_files(capsys):
+    context = {"all_files": []}
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert captured.out == "Total size: 0\n"
